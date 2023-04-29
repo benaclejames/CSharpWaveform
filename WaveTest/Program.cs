98551a54586f8c566d8ec22399050ffd7476b366
benaclejames <benaclejames@gmail.com>
2023-04-30 00:34:14 +0100

    Initial Commit

---
+++ WaveTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace WaveTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Open the wav declared in our args
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: WaveTest.exe <wav file> <output file>");
+                return;
+            }
+            
+            byte[] file = null;
+            try
+            {
+                file = File.ReadAllBytes(args[0]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not open file");
+            }
+            if (file == null || file.Length < 44)
+            {
+                Console.WriteLine("File is not a valid wav file.");
+                return;
+            }
+
+            var parser = new WaveParser(new BinaryReader(new MemoryStream(file)));
+            
+            // Now we need to find the average volume of every quarter second of audio
+            float[] averageVolume = new float[parser.Length*8];
+            for (int segment = 0; segment < parser.Length*8; segment++) {
+                // For every quarter chunk, read byte by byte and add to the average volume for this quarter
+                int quarterLength = parser.ByteRate / 8;
+                byte[] quarterBytes = parser.ReadSamples(quarterLength);
+                float quarterVolume = quarterBytes.Sum(x => (float) x);
+                averageVolume[segment] = quarterVolume / (parser.ByteRate/8);
+            }
+
+            // Write the average volume to a file for python to plot
+            File.WriteAllLines(args[1], Array.ConvertAll(averageVolume, x => x.ToString()));
+        }
+    }
+}
---
+++ WaveTest/WaveParser.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+
+namespace WaveTest
+{
+    public class WaveParser
+    {
+        private BinaryReader _reader;
+        
+        public int FileSize { get; private set; }
+        public int FormatSize { get; private set; }
+        public short FormatType { get; private set; }
+        public short Channels { get; private set; }
+        public int SampleRate { get; private set; }
+        public int ByteRate { get; private set; }
+        public short BlockAlign { get; private set; }
+        public short BitsPerSample { get; private set; }
+        public int DataSize { get; private set; }
+        public int Length { get; private set; }
+        
+        public WaveParser(BinaryReader reader)
+        { 
+            _reader = reader;
+            
+            // First off, we ensure this is a valid RIFF file
+            var riff = new byte[4];
+            reader.Read(riff, 0, 4);
+            if (riff[0] != 'R' || riff[1] != 'I' || riff[2] != 'F' || riff[3] != 'F')
+            {
+                throw new InvalidDataException("Not a valid RIFF file");
+            }
+            
+            // Next, we read the file's size
+            FileSize = reader.ReadInt32();
+
+            // Next, we ensure this is a valid WAVE file
+            var wave = new byte[4];
+            reader.Read(wave, 0, 4);
+            if (wave[0] != 'W' || wave[1] != 'A' || wave[2] != 'V' || wave[3] != 'E')
+            {
+                throw new InvalidDataException("Not a valid WAVE file");
+            }
+            
+            // Ensure fmt is next
+            var fmt = new byte[4];
+            reader.Read(fmt, 0, 4);
+            if (fmt[0] != 'f' || fmt[1] != 'm' || fmt[2] != 't' || fmt[3] != ' ')
+            {
+                throw new InvalidDataException("Not a valid fmt chunk");
+            }
+
+            // More metadata about the samples themselves
+            FormatSize = reader.ReadInt32();
+            FormatType = reader.ReadInt16();
+            Channels = reader.ReadInt16();
+            SampleRate = reader.ReadInt32();
+            ByteRate = reader.ReadInt32();
+            BlockAlign = reader.ReadInt16();
+            BitsPerSample = reader.ReadInt16();
+            
+            // Now we ensure "data" is next
+            var data = new byte[4];
+            reader.Read(data, 0, 4);
+            if (data[0] != 'd' || data[1] != 'a' || data[2] != 't' || data[3] != 'a')
+            {
+                throw new InvalidDataException("Not a valid data chunk");
+            }
+            
+            DataSize = reader.ReadInt32();
+
+            Length = DataSize / ByteRate;
+        }
+        
+        public byte[] ReadSamples(int count)
+        {
+            var samples = new byte[count];
+            _reader.Read(samples, 0, count);
+            return samples;
+        }
+    }
+}
